00d5767c1f4c879d337fac818ae34670a55a50e1
Marcia307 <tysonm4525@student.faytechcc.edu>
2024-09-04 17:58:32 +0000

    Changed the file

---
+++ P1LAB1.py
@@ -0,0 +1,12 @@
+# Marcia Tyson
+# September 4, 2024
+# P1LAB1
+# Shows how to get input from the user and display output.
+
+# The input function always saves the data as a Stirng by default
+First_Name= input("Enter your First Name: ")
+Last_Name= input("Enter your Last Name: ")
+print()
+print("Hello", First_Name, Last_Name +"!", "Welcome to CTI-110")
+print()
+print(type(First_Name))
