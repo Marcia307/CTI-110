fc58e226adc78f39b707a6b76dd3326a59a0f914
Marcia307 <tysonm4525@student.faytechcc.edu>
2024-09-04 18:33:19 +0000

    Changed the file

--- P1HW1.py
+++ P1HW1.py
@@ -1,1 +1,22 @@
+# Marcia Tyson
+# September 4, 2024
+# P1HW1
+# Input and Output with Integers
 
+print("----Caluclating Exponents----")
+print()
+print()
+base_value=int(input("Enter an interger as the base value: "))
+exponent=int(input("Enter an integer as the exponent: "))
+print()
+print()
+print(base_value,"raised to the power of",exponent,"is", base_value**exponent,"!!")
+print()
+print()
+print("----- Adition and Subtraction-----")
+my_num=int(input("Enter a starting integer: "))
+add=int(input("Enter an integer to add: "))
+subtract=int(input("Enter an integer to subtract: "))
+print()
+print()
+print(my_num,"+",add,"-",subtract,"is eqaul to",my_num+add-subtract)
