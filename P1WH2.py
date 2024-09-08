5df2e789629e78d3fbe69a3219792cab445e3581
Marcia307 <tysonm4525@student.faytechcc.edu>
2024-09-07 23:26:26 -0400

    Update P1WH2.py

--- P1WH2.py
+++ P1WH2.py
@@ -3,3 +3,19 @@
 # P1HW2
 # Create a program that does some basic math on numbers that are entered
 
+print("This Program Claculates and dipsplay trvel expense")
+print()
+budget_value=int(input("Enter Budget: "))
+travel_destination=int(input("Enter your travel destination: "))
+exponent=int(input("Enter an integer as the exponent: "))
+exponent=int(input("Enter an integer as the exponent: "))
+exponent=int(input("Enter an integer as the exponent: ")
+print()
+print()
+print("------------Travel Expenses------------")
+my_num=int(input("Enter a starting integer: "))
+add=int(input("Enter an integer to add: "))
+subtract=int(input("Enter an integer to subtract: "))
+print()
+print()
+print(my_num,"+",add,"-",subtract,"is eqaul to",my_num+add-subtract)
